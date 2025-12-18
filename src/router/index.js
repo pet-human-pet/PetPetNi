0b30208bac6eba1dc74dc3bf2e019f61ab806e0d
jack <kartg0052598@gmail.com>
2025-12-19 00:43:52 +0800

    chore: 設定 Profile 頁面路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import ProfileView from '../views/Profile.vue' // 引入你的新檔案
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: ProfileView
+    }
+  ],
 })
 
 export default router
