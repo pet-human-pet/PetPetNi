b4f1321885ff3d9ac0ce802b699d26a484c6e955
Louis-369 <9313louis@gmail.com>
2025-12-22 23:25:06 +0800

    chore:設定 ESLint, Prettier 與 Tailwind 自動排序規範

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,45 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
 
-export default defineConfig([
+export default [
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**', '*.d.ts'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  js.configs.recommended,
+
+
+  ...pluginVue.configs['flat/recommended'],
+
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*'],
+  },
+
 
   {
     languageOptions: {
       globals: {
         ...globals.browser,
+        ...globals.node,
       },
+    },
+    rules: {
+      // 團隊共識規則 
+      'vue/multi-word-component-names': 'off', // 關閉元件名稱雙字限制
+      'vue/require-v-for-key': 'error',        // v-for 一定要 key
+      'no-unused-vars': 'warn',                // 沒用到的變數給警告
+      'no-var': 'error',                       // 禁止使用 var
+      'no-console': 'warn',                    // console.log 給警告
     },
   },
 
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  skipFormatting,
-])
+  // 【關鍵】載入 Prettier 設定 (一定要放最後！)
+  // 這會關閉所有跟排版衝突的 ESLint 規則
+  eslintConfigPrettier,
+]
---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: false,          
+  singleQuote: true,      
+  printWidth: 100,       
+  trailingComma: 'none', 
+  
+  plugins: ['prettier-plugin-tailwindcss'],
+}
