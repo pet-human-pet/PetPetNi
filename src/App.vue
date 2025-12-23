f5820b84d4739891ba0e74b3fba7b1c2a993f283
Louis-369 <9313louis@gmail.com>
2025-12-23 15:24:06 +0800

    chore:初次首頁切版

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-import SocialView from './views/SocialView.vue';
+import { RouterView } from "vue-router";
+import AppHeader from "./components/Share/AppHeader.vue";
+import AppFooter from "./components/Share/AppFooter.vue";
+import MenuOverlay from "./components/Share/MenuOverlay.vue";
 </script>
 
 <template>
-  <div>
-    <SocialView />
+  <div class="relative w-full h-full">
+    <MenuOverlay />
+
+    <AppHeader />
+
+    <main class="w-full h-full">
+      <RouterView />
+    </main>
+    <AppFooter />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import './style.css'
 import App from './App.vue'
 import router from './router'
+import './style.css'
 
 const app = createApp(App)
 
-// 告訴 App 使用 Pinia (狀態管理)
 app.use(createPinia())
-
-// 告訴 App 使用 Router (路由)
 app.use(router)
 
 app.mount('#app')
