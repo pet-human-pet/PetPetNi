5c9ba2bb18b199599026b35a72bcecbefced2003
Louis-369 <9313louis@gmail.com>
2025-12-26 09:46:01 +0800

    chore:調整button 顯示邏輯

--- src/components/Share/AppHeader.vue
+++ src/components/Share/AppHeader.vue
@@ -8,7 +8,6 @@ const uiStore = useUIStore()
   <header
     class="pointer-events-none fixed top-0 left-0 z-100 flex w-full items-start justify-between px-9 pt-[30px] pb-6 md:px-14 md:pt-[40px]"
   >
-    <!-- Logo Area (Pointer events auto to click) -->
     <div
       class="pointer-events-auto shrink-0 transition-opacity duration-300"
       :class="{ 'pointer-events-none opacity-0': uiStore.isMenuOpen }"
@@ -20,13 +19,12 @@ const uiStore = useUIStore()
             'text-black': !uiStore.isMenuOpen,
             'text-white': uiStore.isMenuOpen
           }"
-          >技術と熱で世界を変える</span
         >
+          技術と熱で世界を変える
+        </span>
 
-        <!-- Logo Swap: Show specific SVG based on menu state -->
         <div class="relative h-6 w-32 md:h-10 md:w-48">
           <Transition name="fade" mode="out-in">
-            <!-- Menu Open: White Logo -->
             <img
               v-if="uiStore.isMenuOpen"
               src="@/assets/images/logo-white.svg"
@@ -44,9 +42,7 @@ const uiStore = useUIStore()
       </router-link>
     </div>
 
-    <!-- Menu Button -->
     <div class="pointer-events-auto mr-0 flex shrink-0 items-center gap-2 md:mr-6 md:gap-4">
-      <!-- Recruitment Button -->
       <a
         v-if="$route.path !== '/login'"
         href="#"
@@ -56,7 +52,6 @@ const uiStore = useUIStore()
         募集要項
       </a>
 
-      <!-- Entry Button -->
       <a
         v-if="$route.name !== 'login'"
         href="#"
@@ -66,7 +61,6 @@ const uiStore = useUIStore()
         ENTRY
       </a>
 
-      <!-- Login Button -->
       <router-link
         v-if="$route.name !== 'login'"
         to="/login"
@@ -85,13 +79,12 @@ const uiStore = useUIStore()
         class="bg-mayekawa absolute inset-0 origin-center scale-0 rounded-full opacity-10 transition-transform duration-300 group-hover:scale-100"
       ></div>
 
-      <!-- Menu Text -->
       <span
         class="text-mayekawa group-hover:text-mayekawa mb-0.5 text-[8px] font-bold transition-colors md:mb-1 md:text-[10px]"
-        >MENU</span
       >
+        MENU
+      </span>
 
-      <!-- Hamburger Icon -->
       <div
         class="relative flex h-3 w-5 flex-col items-center justify-between transition-all duration-300 md:h-4 md:w-6"
         :class="{ 'rotate-180': uiStore.isMenuOpen }"
