299847bd455faeaa879129661212189fef30246a
153ys <daphnekao888@gmail.com>
2025-12-31 12:39:33 +0800

    feat: 新增圖片預覽modal

---
+++ src/composables/useImagePreview.js
@@ -0,0 +1,28 @@
+// src\components\Share\ImagePreviewModal.vue
+import { ref } from 'vue'
+
+export const useImagePreview = () => {
+  const previewOpen = ref(false)
+  const previewImages = ref([])
+  const previewIndex = ref(0)
+
+  const openPreview = ({ images, index }) => {
+    previewImages.value = images ?? []
+    previewIndex.value = index ?? 0
+    previewOpen.value = true
+  }
+
+  const closePreview = () => {
+    previewOpen.value = false
+    previewImages.value = []
+    previewIndex.value = 0
+  }
+
+  return {
+    previewOpen,
+    previewImages,
+    previewIndex,
+    openPreview,
+    closePreview
+  }
+}
