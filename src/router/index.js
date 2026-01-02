697509985ec1dcb07537557823c72f57b57d1e65
mila1222 <mila19971222@gmail.com>
2026-01-02 14:30:14 +0800

    modify: 調整meta使聊天室隱藏footer

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,10 @@ const router = createRouter({
       path: '/chat-test',
       name: 'chat-test',
       component: () => import('@/views/ChatRoomView.vue'),
-      meta: { headerType: 'none' }
+      meta: { 
+        headerType: 'none',
+        hideFooter: true
+      }
     },
 
     {
