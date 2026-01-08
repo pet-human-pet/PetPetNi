c8e1119cdbc68792023cb589b1bef3a48e04a492
mila1222 <mila19971222@gmail.com>
2026-01-08 14:23:46 +0800

    feat: 頁面新import使用元件並調整全域通知順序

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,19 @@ import AppHeader from './components/Share/AppHeader.vue'
 import MainHeader from './components/Share/Header.vue'
 import AppFooter from './components/Share/AppFooter.vue'
 import MenuOverlay from './components/Share/MenuOverlay.vue'
+import ConfirmDialog from './components/Share/ConfirmDialog.vue'
+import ReportDialog from './components/Share/ReportDialog.vue'
 import SimpleToast from './components/Share/SimpleToast.vue'
 
 const route = useRoute()
-
-// 2. 計算是否顯示 Footer
-// 如果 meta.hideFooter 為 true，則不顯示
 const showFooter = computed(() => !route.meta.hideFooter)
 </script>
 
 <template>
   <div class="relative flex min-h-screen w-full flex-col">
+    <SimpleToast />
+    <ConfirmDialog />
+    <ReportDialog />
     <MenuOverlay />
     <AppHeader v-if="route.meta.headerType === 'landing'" />
     <MainHeader v-if="route.meta.headerType === 'app'" />
