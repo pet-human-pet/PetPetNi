4e267bde9bc528ecf5d094e25b5e28364fed9d16
mila1222 <mila19971222@gmail.com>
2026-01-09 07:59:47 +0800

    feat: Axios新增TODO

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-// 1.建立 axios 實體
-// 建立一個專屬的api物件
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 10000, 
@@ -10,27 +8,23 @@ const api = axios.create({
   }
 })
 
-// 2. Request Interceptor: 自動帶入 Token
-// 每次發送請求前，這個函式都會被執行
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token') // 假設 Token 存在這裡，從localStorage 拿 Token
+    const token = localStorage.getItem('token') // TODO:之後改useAuthStore取得。並或可在src/utils/constants.js定義 export const TOKEN_KEY ='auth_token'引用
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
-    } // 如果有Token，就把它貼在Header
+    } 
     return config
   },
   (error) => Promise.reject(error)
 )
 
-// 3. Response Interceptor: 統一錯誤處理
-// 每次收到後端的回應後，會先經過這裡
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
       console.warn('Token expired or unauthorized')
-      // TODO: 之後這裡可以加會導向登入頁或是哪裡的邏輯
+      // TODO: 之後這裡可以加導向登入頁或是哪裡的邏輯
     }
     return Promise.reject(error)
   }
