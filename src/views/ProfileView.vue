51b86578a46a954b2ea4c39aa2973eb4a5991940
jack <kartg0052598@gmail.com>
2026-01-10 21:40:12 +0800

    style: 統一在各裝置的上間距

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -246,7 +246,7 @@ onUnmounted(() => {
     <BackgroundGrid />
 
     <div
-      class="mx-auto flex w-full max-w-7xl flex-1 justify-center overflow-visible px-2 pt-10 pb-6 min-[800px]:pt-[70px] md:px-4 md:pb-24 lg:overflow-hidden lg:pt-10"
+      class="mx-auto flex w-full max-w-7xl flex-1 justify-center overflow-visible px-2 pt-10 pb-6 md:px-4 md:pb-24 lg:overflow-hidden"
     >
       <div
         class="border-border-default/20 flex h-full w-full flex-col items-stretch overflow-visible rounded-3xl border bg-white text-left shadow-sm lg:grid lg:grid-cols-[1.2fr_2fr] lg:gap-10 lg:overflow-hidden lg:border-none lg:bg-transparent lg:shadow-none"
@@ -421,9 +421,7 @@ onUnmounted(() => {
           <div
             class="sticky top-10 z-40 -mx-2 flex-none border-b border-gray-100 bg-transparent px-2 min-[800px]:top-[70px] md:-mx-4 md:px-4 lg:static lg:z-auto lg:mx-0 lg:rounded-t-3xl lg:border-b-0 lg:bg-white lg:px-0"
           >
-            <div class="mt-2 h-px w-full bg-gray-100 lg:hidden"></div>
-
-            <div class="flex shrink-0 justify-around px-4 pt-2 lg:px-6 lg:pt-4">
+            <div class="flex shrink-0 justify-around px-4 pt-4 lg:px-6">
               <button
                 v-for="tab in [
                   { id: 'posts', n: '貼文' },
