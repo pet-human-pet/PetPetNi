0423ffd2f9e4b17c72fba153ee735d3b21e1d0d5
Louis-369 <9313louis@gmail.com>
2026-01-28 12:58:32 +0800

    fix:邏輯修復：修正 getShuffledImages洗牌演算法的 ReferenceError。

--- src/views/DailyMatchView.vue
+++ src/views/DailyMatchView.vue
@@ -133,10 +133,8 @@ const tarotImages = Object.values(tarotModules)
 function getShuffledImages(images, count) {
   const shuffled = [...images]
   for (let i = shuffled.length - 1; i > 0; i--) {
-    const j = (Math.floor(Math.random() * (i + 1))[(shuffled[i], shuffled[j])] = [
-      shuffled[j],
-      shuffled[i]
-    ])
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
   }
   return shuffled.slice(0, count)
 }
